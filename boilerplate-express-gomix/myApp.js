86b298af1f0ce24207b1ed07215376d40cd79fe1
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2021-04-20 07:02:29 +0100

    updated

--- boilerplate-express-gomix/myApp.js
+++ boilerplate-express-gomix/myApp.js
@@ -2,13 +2,18 @@ var express = require('express');
 var app = express();
 
 
+console.log("Hello World");
+//Standard GET Req
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// });
 
-
-
-
-
-
-
+//Use the styles
+app.use("/public", express.static(__dirname + "/public"));
+//Get the views
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 
 
