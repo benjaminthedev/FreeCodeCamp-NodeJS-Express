0f9cd6128827f7a5e08613f22134c3c8438b1c92
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2021-04-22 12:53:22 +0100

    make it better

--- boilerplate-express-gomix/myApp.js
+++ boilerplate-express-gomix/myApp.js
@@ -1,14 +1,6 @@
 var express = require('express');
 var app = express();
 
-
-
-console.log("Hello World");
-//Standard GET Req
-// app.get("/", (req, res) => {
-//   res.send("Hello Express");
-// });
-
 //Use the styles
 app.use("/public", express.static(__dirname + "/public"));
 //Get the views
@@ -17,15 +9,13 @@ app.get("/", function(req, res) {
 });
 //App GET json data
 app.get("/json", (req, res) => {
-
-  console.log('working?');
     if (process.env.MESSAGE_STYLE === "uppercase") {
         res.json({
           message: "HELLO JSON"
         });
     } else {
         res.json({
-          message: "Hello Json"
+          message: "Hello json"
         });
     }
 });
