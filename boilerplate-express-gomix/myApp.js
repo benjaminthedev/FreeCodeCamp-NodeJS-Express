9a20fd7bae2a16a774326798f86970b4a152f8e8
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2021-04-23 21:13:35 +0100

    updated

--- boilerplate-express-gomix/myApp.js
+++ boilerplate-express-gomix/myApp.js
@@ -1,12 +1,35 @@
 var express = require('express');
 var app = express();
 
+// Middlewear
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip)
+    //console.log(req.method)
+    //console.log(req.ip)
+  next()
+});
+
+
 //Use the styles
 app.use("/public", express.static(__dirname + "/public"));
 //Get the views
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+
+// App GET /now
+//Chaning Middlewear - A timed server
+app.get('/now', (req, res, next) => {
+     req.time = new Date().toString();
+     next()
+}, (req, res) => {
+    res.json({
+      'time': req.time
+    })
+});
+
+
 //App GET json data
 app.get("/json", (req, res) => {
 
